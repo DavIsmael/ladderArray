bd2e0b19b0e7a25491a45af014d10f9329bc10fa
Davvi <95988082+DavIsmael@users.noreply.github.com>
2024-01-30 00:04:32 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+public class main {
+    public static void main(String[] args) throws IOException{
+        int[][] fallafel = new int[5][];
+        fallafel[0] = new int[1];
+        fallafel[1] = new int[2];
+        fallafel[2] = new int[3];
+        fallafel[3] = new int[4];
+        fallafel[4] = new int[5];
+        int l = 76;
+        int k = 7;
+        for(int i = 0; i < fallafel.length; ++i){
+            for(int j = 0; j < i + 1; ++j, l += 16, k += l){
+                fallafel[i][j] = k;
+            }
+        }
+        for(int i = 0; i < fallafel.length; ++i){
+            for(int j = 0; j < i + 1; ++j){
+                System.out.print(fallafel[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
